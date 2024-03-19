17d8adb7622e909a1bbb5f854999cfc9ce3a27b2
devhoody <rethnoly@gmail.com>
2024-03-19 18:45:30 +0900

    feat: first commit

---
+++ src/main/java/com/allmap/AllmapApplication.java
@@ -0,0 +1,13 @@
+package com.allmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllmapApplication.class, args);
+    }
+
+}
