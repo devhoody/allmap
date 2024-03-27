856bb8f2e5cd68d273b4510d7fc6d1b8a3639bff
devhoody <rethnoly@gmail.com>
2024-03-27 16:24:24 +0900

    feat: Add 식당목록조회 테스트 추가

--- src/test/java/com/allmap/repository/MemoryRestaurantRepositoryTest.java
+++ src/test/java/com/allmap/repository/MemoryRestaurantRepositoryTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemoryRestaurantRepositoryTest {
@@ -26,10 +29,26 @@ class MemoryRestaurantRepositoryTest {
         restaurantRepository.save(1L, restaurant);
         Restaurant findRestaurant = restaurantRepository.findById(1L);
         //then
-        Assertions.assertThat(findRestaurant.getName()).isEqualTo("평양냉면");
+        assertThat(findRestaurant.getName()).isEqualTo("평양냉면");
     }
 
     @Test
     void findById() {
+    }
+
+    @Test
+    @DisplayName("모든 식당 출력")
+    void 식당리스트출력(){
+        //given
+        Restaurant restaurant1 = new Restaurant(1L,"restA","마포구","호사장");
+        Restaurant restaurant2 = new Restaurant(2L,"restB","마포구","호사장");
+        restaurantRepository.save(1L, restaurant1);
+        restaurantRepository.save(2L, restaurant2);
+
+        //when
+        List<Restaurant> list = restaurantRepository.findByAll();
+
+        //then
+        assertThat(list.size()).isEqualTo(2);
     }
 }
